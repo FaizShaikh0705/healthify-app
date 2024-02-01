8fbb96a42f315dcae472040b97e37ccd0d581ad7
FaizShaikh0705 <faizshaikh0705@gmail.com>
2024-02-01 13:09:25 +0530

    server created

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-const server = () => {
-  return (
-    <div>server</div>
-  )
-}
+dotenv.config();
+const app = express()
 
-export default server
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('DB Connection Sucessfull'))
+    .catch((err) => {
+        console.log(err);
+    })
+
+app.use(cors());
+app.use(express.json())
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => {
+    console.log('Backend server is running', PORT)
+})
